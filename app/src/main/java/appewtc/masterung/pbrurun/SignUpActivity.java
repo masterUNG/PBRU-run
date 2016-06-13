5b2968d33b1fc52871c4f6fa735ee409d0ca19aa
masterUNG <phrombutr@gmail.com>
2016-06-13 15:18:33 +0700

    bind

--- app/src/main/java/appewtc/masterung/pbrurun/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/pbrurun/SignUpActivity.java
@@ -1,14 +1,49 @@
 package appewtc.masterung.pbrurun;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton avata0RadioButton, avata1RadioButton,
+            avata2RadioButton, avata3RadioButton, avata4RadioButton;
+    private String nameString, userString, passwordString, avataString;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        bindWidget();
+
+
+    }   // Main Method
+
+    private void bindWidget() {
+
+        nameEditText = findViewById(R.id.editText);
+        userEditText = findViewById(R.id.editText2);
+        passwordEditText = findViewById(R.id.editText3);
+        radioGroup = findViewById(R.id.ragAvata);
+        avata0RadioButton = findViewById(R.id.radioButton);
+        avata1RadioButton = findViewById(R.id.radioButton2);
+        avata2RadioButton = findViewById(R.id.radioButton3);
+        avata3RadioButton = findViewById(R.id.radioButton4);
+        avata4RadioButton = findViewById(R.id.radioButton5);
+
+    }   // bindWidget
+
+    public void clickSignUpSign(View view) {
+
+    }   // clickSign
+
+}   // Main Class
