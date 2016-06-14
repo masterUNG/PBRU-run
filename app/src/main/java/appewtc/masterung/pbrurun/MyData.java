4d27733e559c0e610c123f09611477b5bc500749
masterUNG <phrombutr@gmail.com>
2016-06-14 09:34:08 +0700

    Fininsh 13June

---
+++ app/src/main/java/appewtc/masterung/pbrurun/MyData.java
@@ -0,0 +1,15 @@
+package appewtc.masterung.pbrurun;
+
+/**
+ * Created by masterUNG on 6/13/2016 AD.
+ */
+public class MyData {
+
+    //Explicit
+    private int[] avataInts = new int[] {R.drawable.bird48,
+    R.drawable.doremon48, R.drawable.kon48, R.drawable.nobita48, R.drawable.rat48};
+
+    public int[] getAvataInts() {
+        return avataInts;
+    }
+}   // Main Class
--- app/src/main/java/appewtc/masterung/pbrurun/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/pbrurun/SignUpActivity.java
@@ -2,10 +2,12 @@ package appewtc.masterung.pbrurun;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -15,6 +17,7 @@ public class SignUpActivity extends AppCompatActivity {
     private RadioButton avata0RadioButton, avata1RadioButton,
             avata2RadioButton, avata3RadioButton, avata4RadioButton;
     private String nameString, userString, passwordString, avataString;
+    private int indexAnInt = 0;
 
 
     @Override
@@ -25,25 +28,77 @@ public class SignUpActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Radio Controller
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+                    case R.id.radioButton:
+                        indexAnInt = 0;
+                        break;
+                    case R.id.radioButton2:
+                        indexAnInt = 1;
+                        break;
+                    case R.id.radioButton3:
+                        indexAnInt = 2;
+                        break;
+                    case R.id.radioButton4:
+                        indexAnInt = 3;
+                        break;
+                    case R.id.radioButton5:
+                        indexAnInt = 4;
+                        break;
+                }
+
+            }
+        });
+
 
     }   // Main Method
 
     private void bindWidget() {
 
-        nameEditText = findViewById(R.id.editText);
-        userEditText = findViewById(R.id.editText2);
-        passwordEditText = findViewById(R.id.editText3);
-        radioGroup = findViewById(R.id.ragAvata);
-        avata0RadioButton = findViewById(R.id.radioButton);
-        avata1RadioButton = findViewById(R.id.radioButton2);
-        avata2RadioButton = findViewById(R.id.radioButton3);
-        avata3RadioButton = findViewById(R.id.radioButton4);
-        avata4RadioButton = findViewById(R.id.radioButton5);
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        avata0RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        avata1RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        avata2RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        avata3RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        avata4RadioButton = (RadioButton) findViewById(R.id.radioButton5);
 
     }   // bindWidget
 
     public void clickSignUpSign(View view) {
 
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            Toast.makeText(this, "กรุณากรอกทุกช่อง มีช่องว่าง คะ ?",
+                    Toast.LENGTH_SHORT).show();
+
+        } else {
+            //No Space
+            uploadValueToServer();
+        }
+
+
     }   // clickSign
+
+    private void uploadValueToServer() {
+
+        Log.d("pbruV1", "name ==> " + nameString);
+        Log.d("pbruV1", "user ==> " + userString);
+        Log.d("pbruV1", "pass ==> " + passwordString);
+        Log.d("pbruV1", "avata ==> " + Integer.toString(indexAnInt));
+
+
+    }   // upload
 
 }   // Main Class
